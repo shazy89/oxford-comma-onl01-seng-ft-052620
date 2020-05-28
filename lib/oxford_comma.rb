7c356d71169d40014e07aa89f59e12d5d992da6e
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-28 14:56:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,8 +5,10 @@ def oxford_comma(array_to_string)
  elsif  array_to_string.size == 2
     return array_to_string.join(" and ")
     else 
-array_to_string[-1] =  (" and #{array_to_string[-1]}")
-return array_to_string.join (",")
+array_to_string[-1] =  "and #{array_to_string[-1]}"
+return array_to_string.join (", ")
 end
 
 end
+
+

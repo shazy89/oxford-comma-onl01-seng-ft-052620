c4d6b9e911272b0bf13a29bd40323d215396caee
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-28 14:52:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,7 @@
 
-def oxford_comma(array_to_string)
- 
-  if  array_to_string.size == 2
+def oxford_comma(array_to_string, index)
+  array_to_string[index + 1]
+  if  array_to_string == 2
     return array_to_string.join(" and ")
-  elsif
-    array_to_string.size == 3
-    return array_to_string.join (",")
-    
-  else 
-array_to_string[-1] =  (" and #{array_to_string[-1]}")
-return array_to_string.join (",")
-end
 
 end

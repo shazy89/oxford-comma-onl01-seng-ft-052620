b057f1cf016f0f0759869b449c45e1e428756db7
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-28 14:38:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,7 @@
 
-def oxford_comma(array_to_string)
-  if  array_to_string == 1
+def oxford_comma(array_to_string, index)
+  array_to_string[index + 1]
+  if  array_to_string == 2
+    return array_to_string.join(" and ")
+
 end

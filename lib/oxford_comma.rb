a8428ad79f9f92541efd52bf35278511013b9bc8
Ed Shaziman <erdogan_oh@hotmail.com>
2020-05-28 14:59:25 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
-def oxford_comma(array)
+
+def oxford_comma(array_to_string)
+ if array_to_string.size == 1
+   return array_to_string.join
+ elsif  array_to_string.size == 2
+    return array_to_string.join(" and ")
+    else 
+array_to_string[-1] =  "and #{array_to_string[-1]}"
+return array_to_string.join (", ")
+end
 
 end
+
+
